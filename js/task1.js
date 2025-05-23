d59a6b83da2e482e44ad7093dcc093f19dd8e7f2
katyaivv <katya.ivv0@gmail.com>
2025-05-23 14:48:07 +0500

    task1(4-7)

--- js/task1.js
+++ js/task1.js
@@ -34,18 +34,72 @@
 
 // Дано трехзначное число.
 // а) Верно ли, что все его цифры одинаковые?
+// {
+//     let num, n1, n2, n3;
+//     num = parseInt(prompt('введите трехзначное число', 0));
+//     n1 = Math.floor(num / 100);
+//     n2 = Math.floor(num / 10 % 10);
+//     n3 = num % 10;
+
+//     if (n1 == n2 == n3){
+//         alert('YES')
+//     }
+//     else{
+//         alert('NO')
+//     }
+// }
+
+
+// Найти:
+// г) среднее арифметическое всех целых чисел от a до b (значения a и b вводятся с клавиатуры; b =>a).
+// {
+//     let a, b, coun, sred, s;
+//     coun = 0
+//     s = 0
+//     a = parseInt(prompt('от', 0))
+//     b = parseInt(prompt('до', 0))
+//     while (a <= b){
+//         coun += a;
+//         ++a
+//         ++s
+//     }
+//     sred = coun / s;
+//     alert(sred)
+// }
+
+// Дано натуральное число. Определить:
+// г) сколько раз в нем встречаются цифры x и y.
+// {
+//     let num, coun_x, coun_y, x, y;
+//     coun_x = 0
+//     coun_y = 0
+//     num = parseInt(prompt('введите число', 0))
+//     x = parseInt(prompt('введите число x', 0))
+//     y = parseInt(prompt('введите число y', 0))
+//     while (num > 0){
+//         if (num % 10 == x){
+//             ++coun_x
+//         }
+//         if (num % 10 == y){
+//             ++coun_y
+//         }
+        
+//         num = Math.floor(num / 10)
+//     }
+//     console.log('x встречается ' + coun_x )
+//     console.log('y встречается ' + coun_y)
+// }
+
+// Известны оценки ученика по 10 предметам. Определить среднюю оценку.
 {
-    let num, n1, n2, n3;
-    num = parseInt(prompt('введите трехзначное число', 0));
-    n1 = Math.floor(num / 100);
-    n2 = Math.floor(num / 10 % 10);
-    n3 = num % 10;
-
-    if (n1 == n2 == n3){
-        alert('YES')
+    let num, sum, sr, a;
+    num = 1;
+    sum = 0;
+    while (num <= 10){
+        a = parseInt(prompt('введите оченку по вредмету' + num, 0))
+        sum += a;
+        ++num;
     }
-    else{
-        alert('NO')
-    }
+    sr = sum / 10
+    alert(sr)
 }
-
